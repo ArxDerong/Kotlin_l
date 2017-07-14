5102e20dd3a003c5a17a0d8521530bd1af4b8e28
derong218 <derong218@qq.com>
2017-07-14 11:34:19 +0800

    Chapter3

---
+++ Charpter1/src/com/arx/kotlin/chapter3/Main.kt
@@ -0,0 +1,12 @@
+package com.arx.kotlin.chapter3
+
+/**
+ * Created by Arx on 2017/7/14.
+ * derong218@qq.com
+ */
+const val FINAL_HELL_WORLD:String = "HelloWorld"
+var helloWorld:String = FINAL_HELL_WORLD
+val FINAL_HELLO_CHINA="HelloChina"
+fun main(args: Array<String>) {
+    println("Hello ${args[0]}")
+}
